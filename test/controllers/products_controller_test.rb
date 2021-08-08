a176f926e907f2b1c3a9e72e6382f9a94551c84b
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-08-08 13:31:03 -0700

    pg 106 playtime section

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -52,4 +52,13 @@ class ProductsControllerTest < ActionController::TestCase
 
     assert_redirected_to products_path
   end
+
+  test "should verify products page" do
+    get :products
+    assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
 end
